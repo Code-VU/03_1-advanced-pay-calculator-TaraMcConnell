778ecacea9ea757decc6a6eaf4590462dcf9f0a4
Tara McConnell <Tara.McConnell@vu.com>
2023-01-31 20:40:13 -0600

    Input hours and hourly rate to find gross pay

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,28 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hours = input("Enter Hours:")
+    hours = float(hours)
+
+    hourly_rate = input("Enter Rate: ")
+    hourly_rate = float(hourly_rate)
     
+    if hours > 40: 
+        #logic that reads overtime
+        overtime_hours = hours - 40 
+        normal_hours = 40
+
+        overtime_pay = overtime_hours * hourly_rate * 1.5
+        normal_pay = normal_hours * hourly_rate
+        
+        gross_pay = normal_pay + overtime_pay
+
+    else:
+        #the usual
+        gross_pay = hours * hourly_rate
+
+        print(f"Pay: (gross_pay)")
+    
+
     # end assignment
 
 ## if you want to test locally before you try to sync
